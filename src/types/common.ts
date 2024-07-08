bb5b188b83d5175b5ebe4e5800b48dfdb809fb7e
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-08 16:34:34 +0700

    feat: add types and util function

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,6 +4,9 @@ import type {
   Member as _Member,
   Profile as _Profile,
   MemberRole as _MemberRole,
+  Message as _Message,
+  DirectMessage as _DirectMessage,
+  Conversation as _Conversation,
 } from "@prisma/client";
 
 export type Member = _Member & {
@@ -14,6 +17,20 @@ export type Channel = _Channel;
 
 export type Server = _Server & {
   members: Member[];
+};
+
+export type Message = _Message & {
+  member: Member;
+};
+
+export type DirectMessage = _DirectMessage & {
+  member: Member;
+};
+
+export type Conversation = _Conversation & {
+  directMessage?: DirectMessage[];
+  memberOne: Member;
+  memberTwo: Member;
 };
 
 export type MemberRole = _MemberRole;
