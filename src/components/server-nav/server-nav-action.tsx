95c3fbec40877246aae8354680b88418dc7b8531
linhnvh15vn <linhnvh15vn@gmail.com>
2024-11-27 14:17:02 +0700

    feat: open server modal when click create server on channel navigation

--- src/components/server-nav/server-nav-action.tsx
+++ src/components/server-nav/server-nav-action.tsx
@@ -1,16 +1,22 @@
+"use client";
+
 import React from "react";
 
 import { Plus } from "lucide-react";
 
 import CustomTooltip from "~/components/custom-tooltip";
+import { useModalStore } from "~/store/use-modal-store";
 
 export default function ServerNavAction() {
+  const { onOpen } = useModalStore();
+
   return (
     <CustomTooltip side="right" align="center" label="Tạo máy chủ">
       <div className="group">
         <button
           type="button"
           className="flex size-12 items-center justify-center rounded-3xl bg-accent transition-all group-hover:rounded-2xl group-hover:bg-emerald-500"
+          onClick={() => onOpen("SERVER", {})}
         >
           <Plus className="text-emerald-500 transition-all group-hover:text-white" />
         </button>
